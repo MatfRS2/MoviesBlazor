d2809051f4ad6aa8c26dc4c2be6ef3e29c3ee402
Vladimir Filipovic <vladofilipovic@hotmail.com>
2022-11-10 14:57:51 +0100

    dodao razor biblioteku za zajednicke kompnente

---
+++ MoviesRazorClassLibrary/Areas/MyFeature/Pages/Page1.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MoviesRazorClassLibrary.MyFeature.Pages
+{
+    public class Page1Model : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
