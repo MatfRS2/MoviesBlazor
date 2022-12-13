4ee98d07c5e4ba24930ca28470389365054ee8ef
Vladimir Filipovic <vladofilipovic@hotmail.com>
2022-12-13 08:42:21 +0100

    popravio losu lokaciju profila za mapiranje

--- BlazorMovies/MoviesWebApi/Queries/Mappings/MapKorisnikProfile.cs
+++ BlazorMovies/MoviesWebApi/Queries/Mappings/MapKorisnikProfile.cs
@@ -1,14 +1,21 @@
 ﻿using AutoMapper;
 
 using MoviesWebApi.Models;
+using System;
 
-namespace MoviesWebApi.ViewModels.Mappings
+namespace MoviesWebApi.Queries.Mappings
 {
     public class MapKorisnikProfile : Profile
     {
         public MapKorisnikProfile()
         {
-            CreateMap<Korisnik, KorisnikGetDto>();
+            CreateMap<Korisnik, MoviesWebApi.Queries.VratiKorisnikPoId.KorisnikResponse>()
+                .ConstructUsing(x => new MoviesWebApi.Queries.VratiKorisnikPoId.KorisnikResponse(
+                    x.KorisnikId, x.Email, x.Ime, x.Prezime, x.Potroseno));
+            CreateMap<Korisnik, MoviesWebApi.Queries.VratiKorisnikSve.KorisnikResponse>()
+                .ConstructUsing(x => new MoviesWebApi.Queries.VratiKorisnikSve.KorisnikResponse(
+                    x.KorisnikId, x.Email, x.Ime, x.Prezime, x.Potroseno));
         }
     }
 }
+
